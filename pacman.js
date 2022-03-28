ead69fe44ce1f4a3a4a68678f8a61a369302619b
Vida <herling.codingcamp@gmail.com>
2022-03-28 00:06:32 -0400

    add run and stop function

--- pacman.js
+++ pacman.js
@@ -1,36 +1,65 @@
-//pacmen1.js: use setTimeout in the Run() function
+//use setTimeout in the Run() function
 
 var pos = 0;
 const pacArray = [
-    ['PacMan1.png', 'PacMan2.png'],
-    ['PacMan3.png', 'PacMan4.png']
+  ['PacMan1.png', 'PacMan2.png'],
+  ['PacMan3.png', 'PacMan4.png']
 ];
 var direction = 0;
 var focus = 0;
 
-function Run() {
-    let img = document.getElementById("PacMan");
-    let imgWidth = img.width
-    focus = (focus + 1) % 2;
-    direction = checkPageBounds(direction, imgWidth);
-    img.src = pacArray[direction][focus];
-    if (direction) {
-        pos -= 20;
-        img.style.left = pos + "px";
-    } else {
-        pos += 20;
-        img.style.left = pos + 'px';
-    }
-    setTimeout(Run, 200); // Use setTimeout to call Run every 200 millesecs
+function pacmanRun() {
+  let img = document.getElementById("PacMan"); //get a handle of the image
+  let imgWidth = img.width
+  focus = (focus + 1) % 2;
+  direction = checkPageBounds(direction, imgWidth);
+  img.src = pacArray[direction][focus]; //use PacMan pictures in the pacArray array
+  if (direction) {
+      pos -= 20;
+      img.style.left = pos + "px"; //if direction is 1, pacman moves from right to left
+  } else {
+      pos += 20;
+      img.style.left = pos + 'px'; //if direction is 0, pacman moves from left to right
+  }
 }
 
-//my code below
 function checkPageBounds(direction, imgWidth) {
-if(pos + imgWidth >= window.innerWidth){
-direction = 1;
+    if(pos + imgWidth >= window.innerWidth){
+      direction = 1;
+    }
+    if(pos <= 0){
+      direction = 0;
+    } 
+    return direction;
+  }
+
+
+mixBut.addEventListener("click", Start); //button inital status
+
+function Start(){
+    console.log("Started");
+    mixBut.removeEventListener("click", Start)
+    mixBut.addEventListener("click", Stop);
+    mixBut.value = "Stop";
 }
-if(pos <= 0){
-direction = 0;
-} 
-return direction;
+
+function Stop(){
+    console.log("Stopped");
+    mixBut.removeEventListener("click", Stop);
+    mixBut.addEventListener("click", Start);
+    mixBut.value = "Start";
 }
+
+let run; 
+function Run(){
+  if(mixBut.value === "Start"){
+    run = setInterval(pacmanRun, 200);
+  } 
+  if(mixBut.value === "Stop") {
+    clearInterval(run);
+  }
+}
+
+
+
+
